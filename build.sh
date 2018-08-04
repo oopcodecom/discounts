f26b37fddcf09437453127f3ad6e6b7d9ec9ebe2
Bogdan Hmarnii <bogdan.hmarnii@gmail.com>
2018-08-04 21:13:16 +0300

    build script updated

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-dir="/var/www/discounts.local"
-cd "$dir"
-php bin/console composer:install
+composer install
 php bin/console cache:clear
 php bin/console doctrine:schema:create
-php bin/console doctrine:fixtures:load .... yes
+echo "y" | php bin/console doctrine:fixtures:load
