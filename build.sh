ef46ab38c68b0d4fa71c89d60d54622219f3fd6d
Bogdan Hmarnii <bogdan.hmarnii@gmail.com>
2018-08-06 13:57:28 +0300

    fix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker-comsoe build
+docker-compose build
 docker-compose up -d
 docker exec -u dev discounts_php /bin/sh -c "composer install;bin/console d:s:d --full-database --force; bin/console d:s:c;echo 'y' | bin/console d:f:l"
