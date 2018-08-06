3d53313529c4dd2e9063c5c182518326de6cbcb1
Bogdan Hmarnii <bogdan.hmarnii@gmail.com>
2018-08-06 09:32:16 +0300

    docker configuration fixed

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-composer install --prefer-dist
-php bin/console doctrine:schema:create
-echo "y" | php bin/console doctrine:fixtures:load
-php bin/console cache:clear
+composer install -o
